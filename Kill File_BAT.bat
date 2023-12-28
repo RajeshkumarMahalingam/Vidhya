ab9e2848f00b8e8e4f66f1af1a6380969b1d1234
RajeshkumarMahalingam <134055391+RajeshkumarMahalingam@users.noreply.github.com>
2023-12-28 17:50:17 +0530

    Add files via upload

---
+++ Kill File_BAT.bat
@@ -0,0 +1,5 @@
+taskkill /F /IM chromedriver.exe /T
+
+taskkill /F /IM msedge.exe /T
+
+taskkill /F /IM chrome.exe /T
---
+++ SECONDBATCH.bat
@@ -0,0 +1,4 @@
+@echo off
+D:
+cd D:/workspace/Automate_BB15_release/driver
+robot --log logs.html --report reports.html --include=UATSECOND robot_driver
